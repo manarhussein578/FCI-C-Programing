a752940a3d199044f4a0d2ff3d1417c309c73ec6
manarhussein578 <84437175+manarhussein578@users.noreply.github.com>
2021-06-04 21:32:09 +0200

    Add files via upload

---
+++ task.day7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int arr[10];
+   int i=1,m=1,n;
+   for(;m<=10;m++)
+   {
+       scanf("%d",&n);
+   }
+
+   for(;i<=10;i++)
+   {
+       int j=1;
+       for(;j<=10;j++)
+
+           if(arr[i]==arr[j])
+
+       printf("%d",arr[i]);
+   }
+    return 0;
+}
